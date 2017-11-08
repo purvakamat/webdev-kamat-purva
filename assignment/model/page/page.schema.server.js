932eda8c8782129ebbc097058cd50f46fbb990c8
purvakamat <kamatpurva@gmail.com>
2017-11-08 15:15:20 -0500

    page service integrated with mongodb

--- assignment/model/page/page.schema.server.js
+++ assignment/model/page/page.schema.server.js
@@ -1,3 +1,15 @@
 /**
  * Created by kamat on 11/5/2017.
  */
+
+var mongoose = require('mongoose');
+var PageSchema = mongoose.Schema({
+  _website: {type: mongoose.Schema.Types.ObjectId, ref: 'Website'},
+  name: String,
+  title: String,
+  description: String,
+  widgets: [{type: mongoose.Schema.Types.ObjectId, ref: 'Widget'}],
+  dateCreated: Date
+}, {collection: 'Pages'});
+
+module.exports = PageSchema;
--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -9,55 +9,53 @@ module.exports = function (app) {
   app.put("/api/page/:pageId", updatePage);
   app.delete("/api/page/:pageId", deletePage);
 
-  var mockdata = require("./mock.data");
-  var pages = mockdata.pages;
+  //var mockdata = require("./mock.data");
+  //var pages = mockdata.pages;
+  var pageModel = require("../model/page/page.model.server");
 
   function createPage(req, res) {
     var websiteId = req.params['websiteId'];
     var page = req.body;
-    page["websiteId"] = websiteId;
-    page._id = (new Date()).getTime() + "";
-    pages.push(page);
-    res.json(page);
+    pageModel.createPage(websiteId, page).then(function (page) {
+      res.json(page);
+    });
   }
 
   function findAllPagesForWebsite(req, res) {
     var websiteId = req.params['websiteId'];
-    var pagesForWebsite = pages.filter(page => page.websiteId == websiteId);
-    res.json(pagesForWebsite);
+    pageModel.findAllPagesForWebsite(websiteId).then(function (pagesForWebsite) {
+      res.json(pagesForWebsite);
+    });
   }
 
   function findPageById(req, res) {
     var pageId = req.params['pageId'];
-    var page = pages.find(page => page._id == pageId);
-    if(page)
-      res.json(page);
-    else
-      res.status(404).send("No page with given id.");
+    pageModel.findPageById(pageId).then(function (page) {
+      if(page)
+        res.json(page);
+      else
+        res.status(404).send("No page with given id.");
+    });
   }
 
   function updatePage(req, res) {
     var pageId = req.params['pageId'];
     var pageNew = req.body;
-
-    let pIndex = pages.findIndex(p => p._id == pageId);
-    if(pIndex != -1){
-      pages[pIndex] = pageNew;
-      res.json("Page updated");
-    }
-    else
-      res.status(404).send("Page cannot be updated");
+    pageModel.updatePage(pageId, pageNew).then(function (response) {
+      if(response.n >0 || response.nModified > 0)
+        res.json("Page updated");
+      else
+        res.status(404).send("Page was not updated");
+    });
   }
 
   function deletePage(req, res) {
     var pageId = req.params['pageId'];
-
-    let pIndex = pages.findIndex(p => p._id == pageId);
-    if(pIndex != -1){
-      pages.splice(pIndex, 1);
-      res.json("Page deleted");
-    }
-    else
-      res.status(404).send("Page cannot be deleted");
+    pageModel.deletePage(pageId).then(function (response) {
+      if(response.deletedCount > 0)
+        res.json("Page deleted");
+      else
+        res.status(404).send("Page cannot be deleted");
+    });
   }
 };
